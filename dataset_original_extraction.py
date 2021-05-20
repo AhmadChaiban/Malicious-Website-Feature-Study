cb6db801aec8d271039182b7f6e6f7622dcf86b5
Ahmad Chaiban <ahmadchaiban@gmail.com>
2021-05-19 20:12:57 -0400

    Custom feature extraction and more ML

---
+++ dataset_original_extraction.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import requests
+
+pd.options.display.max_columns = 500
+
+dataset = pd.read_csv('dataset_22_mal_new_features.csv')
+dataset_20 = pd.read_csv('dataset_20_new_features.csv').drop(columns = ['Unnamed: 0'])
+
+print(len(dataset))
+
+tlds = dataset_20['tld'].unique()
+
+def check_if_https(url):
+    try:
+        r = requests.get(url)
+        if 'https' in r.url:
+            return 'yes'
+        else:
+            return 'no'
+    except:
+        print('does not work')
+
+
+def get_tld(url):
+    for tld in tlds:
+        if tld in url:
+            return tld
+    return 'no_tld'
+
+dataset['https'] = dataset['url'].apply(lambda x: check_if_https(x))
+dataset['tld'] = dataset['url'].apply(lambda x: get_tld(x))
+
+print(dataset.head())
