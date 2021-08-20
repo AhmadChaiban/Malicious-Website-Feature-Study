f0c1836e8879f7b5fc71e355c9ad92202d7442e3
Ahmad Chaiban <ahmadchaiban@gmail.com>
2021-08-20 13:23:43 -0400

    Code at report level completion

--- preprocessing_ml_table.py
+++ preprocessing_ml_table.py
@@ -8,7 +8,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.decomposition import PCA
-
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
 from xgboost import XGBClassifier
 
 
@@ -71,31 +73,69 @@ def make_classification(features_to_keep, df_surtur):
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=20)
     
-    xgboost_model = XGBClassifier(verbosity=0, 
-                              max_depth=7,
-                              min_child_weight=1,
-                              n_estimators=165,
-                              colsample_bylevel=1,
-                              colsample_bytree=1, 
-                              num_parallel_tree=1,
-                              learning_rate=0.3,
-                              tree_method='exact', 
-                              booster='dart',
-                              gamma=1e-10,
-                              alpha=0,
-                              scale_pos_weight= 1,  # 1.375520774687535,
-                              subsample=1,
-                              n_jobs=-1)
+    param_grid = {
+                'min_child_weight': [1, 5, 10],
+                'gamma': [1, 1e-1, 1e-2, 1e-4, 1e-6, 1e-8, 1e-10],
+                'subsample': [0.6, 0.8, 1.0],
+                'colsample_bytree': [0.6, 0.8, 1.0],
+                'max_depth': [3, 4, 5, 6, 7, 8, 9, 10],
+                'learning_rate': [0.1, 0.2, 0.3, 0.4, 0.5],
+                'n_estimators': [100, 120, 150, 165, 180, 200, 300, 1000],
+                'tree_method': ['auto', 'exact', 'approx', 'hist', 'gpu_hist'],
+                'booster': ['gbtree', 'gblinear', 'dart']
+                
+                }
+    
+#     xgboost_model = XGBClassifier(verbosity=0, 
+#                               max_depth=7,
+#                               min_child_weight=1,
+#                               n_estimators=165,
+#                               colsample_bylevel=1,
+#                               colsample_bytree=1, 
+#                               num_parallel_tree=1,
+#                               learning_rate=0.3,
+#                               tree_method='exact', 
+#                               booster='dart',
+#                               gamma=1e-10,
+#                               alpha=0,
+#                               scale_pos_weight= 1,  # 1.375520774687535,
+#                               subsample=1,
+#                               n_jobs=-1)
 
-    xgboost_model.fit(X_train, y_train)
+    xgboost_model = XGBClassifier()
+
+    folds = 3
+    param_comb = 5
+
+    skf = StratifiedKFold(n_splits=folds, shuffle = True, random_state = 1001)
     
-    y_pred = xgboost_model.predict(X_test)
+    random_search = RandomizedSearchCV(xgboost_model, 
+                                       param_distributions=param_grid, 
+                                       n_iter=param_comb, 
+                                       scoring='roc_auc', 
+                                       n_jobs=-1, 
+                                       cv=skf.split(X_train,y_train), 
+                                       verbose=3, 
+                                       random_state=1001)
+
+    random_search.fit(X_train, y_train)
+
+    # xgboost_model.fit(X_train, y_train)
     
-    print(f'Accuracy_score = {accuracy_score(y_test, y_pred)}')
+    # y_pred = xgboost_model.predict(X_test)
+
+    y_pred_train = random_search.predict(X_train)
+
+    y_pred_test = random_search.predict(X_test)
+    
+    print(f'Train Accuracy_score = {accuracy_score(y_train, y_pred_train)}')
+    print(f'Test Accuracy_score = {accuracy_score(y_test, y_pred_test)}')
     print('\n')
-    print(classification_report(y_test, y_pred, target_names=['benign', 'malicious'], digits=4))
+    print(classification_report(y_test, y_pred_test, target_names=['benign', 'malicious'], digits=4))
     
-    feat_imp = xgboost_model.feature_importances_
+    print(f'best_estimator = {}')
+    
+    feat_imp = random_search.feature_importances_
     feat_dict = {}
 
     for i in range(len(feat_imp)):
